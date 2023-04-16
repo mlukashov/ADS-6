fc83501f9980311ab80ca66c0f9152fe3707fd31
mlukashov <125198939+mlukashov@users.noreply.github.com>
2023-04-16 21:14:42 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,13 +1,14 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
 
 template<typename T, int size>
 class TPQueue {
  private:
     T* arr;
     int last, first, count;
-    
+
  public:
     TPQueue() :first(0), last(0), count(0) { arr = new T[size]; }
     bool isEmpty() const {
